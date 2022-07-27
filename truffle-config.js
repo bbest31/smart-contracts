a4186057f1bbb9ad604a29e77567c32106f5af13
bbest-blockpass <brandon@blockpass.live>
2022-07-27 16:51:36 -0400

    fix: cleaned up the truffle config file

--- truffle-config.js
+++ truffle-config.js
@@ -6,11 +6,6 @@ module.exports = {
       network_id: "*"
     },
     dashboard: {},
-    loc_development_development: {
-      network_id: "*",
-      port: 8545,
-      host: "127.0.0.1"
-    }
   },
   compilers: {
     solc: {
