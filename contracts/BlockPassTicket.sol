1d00177c2f85ff7cde8d155cee543ae203d88177
bbest-blockpass <brandon@blockpass.live>
2023-03-15 20:26:28 -0400

    fix: stack call too deep fix

--- contracts/BlockPassTicket.sol
+++ contracts/BlockPassTicket.sol
@@ -6,33 +6,42 @@ import "@openzeppelin/contracts/utils/introspection/ERC165Storage.sol";
 import "./ABlockPassTicket.sol";
 
 contract BlockPassTicket is ABlockPassTicket, ERC165Storage {
+
+    struct TicketInformation {
+        string tokenURI;
+        uint256 _primarySalePrice;
+        uint8 _secondaryMarkup;
+        uint96 _feeNumerator;
+        uint256 _liveDate;
+        uint256 _closeDate;
+        uint256 _supply;
+    }
+
+    struct EventInformation {
+        address _marketplaceContract;
+        address _eventOrganizer;
+        uint256 _eventEndDate;
+    }
+
     constructor(
-        string memory _name,
-        string memory _symbol,
-        address _marketplaceContract,
-        address _eventOrganizer,
-        string memory tokenURI,
-        uint256 _primarySalePrice,
-        uint8 _secondaryMarkup,
-        uint96 _feeNumerator,
-        uint256 _eventEndDate,
-        uint256 _liveDate,
-        uint256 _closeDate,
-        uint256 _supply
+       string memory _name,
+       string memory _symbol,
+       TicketInformation memory ticketInfo,
+       EventInformation memory eventInfo
     )
         ABlockPassTicket(
             _name,
             _symbol,
-            _marketplaceContract,
-            _eventOrganizer,
-            tokenURI,
-            _primarySalePrice,
-            _secondaryMarkup,
-            _feeNumerator,
-            _eventEndDate,
-            _liveDate,
-            _closeDate,
-            _supply
+            eventInfo._marketplaceContract,
+            eventInfo._eventOrganizer,
+            ticketInfo.tokenURI,
+            ticketInfo._primarySalePrice,
+            ticketInfo._secondaryMarkup,
+            ticketInfo._feeNumerator,
+           eventInfo._eventEndDate,
+            ticketInfo._liveDate,
+           ticketInfo._closeDate,
+            ticketInfo._supply
         )
     {}
 
